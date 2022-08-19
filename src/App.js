7031a54c05b8197b24b661ce26c63d949c375395
shulte <shulte.one@gmail.com>
2022-08-19 13:23:35 -0400

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import app from 'firebase';
-// import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { signInWithGoogle } from './firebase';
 import './style.css';
 
 export default function App() {
-  console.log('getAuth', getAuth);
   return (
     <div className="App">
       <div>Test </div>
-      {/* <button className="login-with-google-btn" onClick={signInWithGoogle}>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in with Google
-      </button> */}
+      </button>
       <h1>Name: {localStorage.getItem('name')}</h1>
       <h1>Email: {localStorage.getItem('email')}</h1>
       <img src={localStorage.getItem('profilePic')} />
--- src/firebase.js
+++ src/firebase.js
@@ -1,36 +1,34 @@
-// import { initializeApp } from 'firebase/app';
-// // import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyDRoqiBEcxu5YPmwqa7bxtRWGwKUjxRFkA',
-//   authDomain: 'shulte-v1.firebaseapp.com',
-//   projectId: 'shulte-v1',
-//   storageBucket: 'shulte-v1.appspot.com',
-//   messagingSenderId: '714891275251',
-//   appId: '1:714891275251:web:4f19b88d8279b401d48086',
-//   measurementId: 'G-F328V4CZ6C',
-// };
+const firebaseConfig = {
+  apiKey: 'AIzaSyDRoqiBEcxu5YPmwqa7bxtRWGwKUjxRFkA',
+  authDomain: 'shulte-v1.firebaseapp.com',
+  projectId: 'shulte-v1',
+  storageBucket: 'shulte-v1.appspot.com',
+  messagingSenderId: '714891275251',
+  appId: '1:714891275251:web:4f19b88d8279b401d48086',
+  measurementId: 'G-F328V4CZ6C',
+};
 
-export const app = {};
-// export const app = initializeApp(firebaseConfig);
-// console.log('initializeApp app', app);
+export const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth(app);
+export const auth = getAuth(app);
 
-// const provider = new GoogleAuthProvider();
+const provider = new GoogleAuthProvider();
 
-// export const signInWithGoogle = () => {
-//   signInWithPopup(auth, provider)
-//     .then((result) => {
-//       const name = result.user.displayName;
-//       const email = result.user.email;
-//       const profilePic = result.user.photoURL;
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      const profilePic = result.user.photoURL;
 
-//       localStorage.setItem('name', name);
-//       localStorage.setItem('email', email);
-//       localStorage.setItem('profilePic', profilePic);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     });
-// };
+      localStorage.setItem('name', name);
+      localStorage.setItem('email', email);
+      localStorage.setItem('profilePic', profilePic);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
